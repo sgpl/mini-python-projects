8559b5e23e2af05aad87062c3f49c8c3a21fbf80
sgpl <sharad45@gmail.com>
2017-08-12 12:53:01 -0400

    Update take_a_break logic to a function.

--- take_a_break.py
+++ take_a_break.py
@@ -1,14 +1,20 @@
-import time 
+#!/usr/bin/python 2.7
+
+import time
 import webbrowser
 
-total_breaks = 3
-break_count = 0
 
-print "This program started on " + time.ctime()
-while break_count < total_breaks:
-	time.sleep(2*60*60) # 2 * 60 * 60 = 2 hours
-	print "The time is now " + time.ctime()
-	print "Get ready to dance...."
-	time.sleep(3) # 
-	webbrowser.open('https://youtu.be/xo1VInw-SKc', new=2)
-	break_count += 1 
+def take_a_break(total_breaks):
+    """Once run, makes the user take breaks depending on the input."""
+    break_count = 0
+    print "This program started on " + time.ctime()
+    while break_count < total_breaks:
+        time.sleep(10)  # 2 * 60 * 60 = 2 hours
+        print "The time is now " + time.ctime()
+        print "Get ready to dance...."
+        time.sleep(3)
+        webbrowser.open('https://youtu.be/xo1VInw-SKc', new=2)
+        break_count += 1
+
+
+take_a_break(3)
